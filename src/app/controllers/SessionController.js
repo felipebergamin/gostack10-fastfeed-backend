c3d808360a4461e220f6b2f4dd2103bb2198642c
Felipe Bergamin <felipebergamin6@gmail.com>
2020-03-17 22:53:50 -0300

    add SessionController with store function

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+
+    const { id, name } = user;
+
+    return res.status(200).json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '4PP_S3CR3T',
+  expiresIn: '7d',
+};
