86c67d5b429c20cc4e60c0225b85f44538ae65e7
Felipe Bergamin <felipebergamin6@gmail.com>
2020-03-17 22:54:03 -0300

    add POST /sessions route to create a token

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const router = new Router();
 
+router.route('/sessions').post(SessionController.store);
 router.route('/users').get(UserController.list);
 
 export default router;
