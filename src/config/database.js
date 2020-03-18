78277cf29f33c6ff7471b679eca90aa264c74b6b
Felipe Bergamin <felipebergamin6@gmail.com>
2020-03-17 22:11:40 -0300

    fix database password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  passord: 'docker',
+  password: 'docker',
   database: 'fastfeed',
   define: {
     timestamps: true,
