fddf60175b956b4245bc58cdf7f298a8f2ec85ac
Felipe Bergamin <felipebergamin6@gmail.com>
2020-03-19 21:51:26 -0300

    add courier model

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,9 @@ import dbConfig from '../config/database';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import Courier from '../app/models/Courier';
 
-const models = [User, Recipient];
+const models = [User, Recipient, Courier];
 
 class Database {
   constructor() {
