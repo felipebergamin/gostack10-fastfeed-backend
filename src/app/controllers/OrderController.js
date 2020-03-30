9e3a8996d5545c507de6481d438aee853c05d05d
Felipe Bergamin <felipebergamin6@gmail.com>
2020-03-30 20:19:34 -0300

    feat: add delete and update methods for orders

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -30,6 +30,35 @@ class OrderController {
     }
   }
 
+  async delete(req, res) {
+    try {
+      const order = await Order.findByPk(req.params.id);
+
+      if (!order) return res.status(404).end();
+
+      await order.destroy();
+
+      return res.status(200).end();
+    } catch (err) {
+      return res.status(500).end();
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const order = await Order.findByPk(req.params.id);
+
+      if (!order) return res.status(404).end();
+
+      order.set(req.body);
+      await order.save();
+
+      return res.json(req.body);
+    } catch (err) {
+      return res.status(500).end();
+    }
+  }
+
   async list(req, res) {
     try {
       const orders = await Order.findAll({
