80d9dcc386ba4ba2ac993257006ef6a3fbbe4886
Felipe Bergamin <felipebergamin6@gmail.com>
2020-04-05 22:21:58 -0300

    feat: add migration for delivery_problems table

---
+++ src/database/migrations/20200406011557-create-delivery_problems.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('delivery_problems', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      order_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'orders',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'RESTRICT',
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('delivery_problems');
+  },
+};
