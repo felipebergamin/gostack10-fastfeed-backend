e27c5ad5fcf6fb27b56c2272ccf4f3222da38c14
Felipe Bergamin <felipebergamin6@gmail.com>
2020-04-05 22:11:11 -0300

    feat: implement delivery controller

---
+++ src/app/controllers/DeliveryController.js
@@ -0,0 +1,36 @@
+import { Op } from 'sequelize';
+
+import Order from '../models/Order';
+import Courier from '../models/Courier';
+
+class DeliveryController {
+  async store(req, res) {
+    const { courierId, orderId } = req.params;
+
+    const courier = await Courier.findByPk(courierId);
+
+    if (!courier) {
+      return res.json(404).json({ message: 'Courier not found' });
+    }
+
+    const order = await Order.findOne({
+      where: {
+        id: orderId,
+        courier_id: courierId,
+        start_date: { [Op.not]: null },
+        end_date: null,
+      },
+    });
+
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+
+    order.set({ end_date: new Date() });
+    await order.save();
+
+    return res.json(order);
+  }
+}
+
+export default new DeliveryController();
